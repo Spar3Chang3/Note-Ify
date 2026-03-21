29a7390bd736264441588ad5d7f7a19527f7aed0
Spar3Chang3 <161397668+Spar3Chang3@users.noreply.github.com>
2026-03-21 17:33:36 -0500

    fixing

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import { Client, GatewayIntentBits } from "discord.js";
 import AppController from "@/lib/controller/AppController.js";
 import {
   DISCORD_TOKEN,
+  WHISPER_URL,
+  WHISPER_MODEL,
+  SUMMARY_MODEL,
+  SUMMARY_PROMPT,
+  CRITIC_PROMPT,
+  FEEDBACK_PROMPT,
+  COLLECTOR_DURATION,
+  MAX_TOKEN_LIMIT,
   LOGIN_ASCII_ART,
   ERR_ASCII_ART,
 } from "@/lib/static/Constants.js";
@@ -24,7 +32,19 @@ client.on("messageCreate", async (message) => {
 
 client
   .login(process.env.DISCORD_TOKEN ?? DISCORD_TOKEN)
-  .then(() =>
-    console.log(`${LOGIN_ASCII_ART}\nLogged in and awaiting vc to join`),
-  )
+  .then(() => {
+    console.log(`${LOGIN_ASCII_ART}\nLogged in and awaiting vc to join`);
+    printValues();
+  })
   .catch((err) => console.error(`${ERR_ASCII_ART}\n`, err));
+
+function printValues() {
+  console.log("Whisper URL:", WHISPER_URL);
+  console.log("Whisper Modell:", WHISPER_MODEL);
+  console.log("Summary Model:", SUMMARY_MODEL);
+  console.log("Summary Prompt:", SUMMARY_PROMPT);
+  console.log("Critic Prompt:", CRITIC_PROMPT);
+  console.log("Feedback Prompt:", FEEDBACK_PROMPT);
+  console.log("Collector Duration:", COLLECTOR_DURATION);
+  console.log("Max Token Limit:", MAX_TOKEN_LIMIT);
+}
